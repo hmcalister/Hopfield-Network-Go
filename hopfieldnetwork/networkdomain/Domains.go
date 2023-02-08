14ec8fe5671410f4c42412451dbcf12be5302c0e
Hayden McAlister <mcaha814@student.otago.ac.nz>
2023-02-08 16:30:34 +1300

    Add documentation to network domains package

--- hopfieldnetwork/networkdomain/Domains.go
+++ hopfieldnetwork/networkdomain/Domains.go
@@ -1,18 +1,23 @@
+// Definitions of the network domains for the Hopfield Network.
 package networkdomain
 
+// Defines the NetworkDomain type as an int32 to work as an Enum.
+//
+// Note this is mainly done to enforce self documenting code.
 type NetworkDomain int32
 
+// The possible Network Domains represented as an Enum.
 const (
-	// Default domain - a sentinel to allow errors if building is attempted
+	// Default domain - a sentinel to allow errors if building is attempted.
 	UnspecifiedDomain NetworkDomain = iota
 
-	// States can only have values in the set {0,1}
+	// States can only have values in the set {0,1}.
 	BinaryDomain NetworkDomain = iota
 
-	// States can only have values in the set {-1,1}
+	// States can only have values in the set {-1,1}.
 	BipolarDomain NetworkDomain = iota
 
-	// States can have any value from the Real Numbers
+	// States can have any value from the Real Numbers.
 	// TODO: Allow continuous but bounded domains
 	ContinuousDomain NetworkDomain = iota
 )
