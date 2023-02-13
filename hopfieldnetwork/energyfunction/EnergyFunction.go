5caf0217875054a29c40d1d69dd3609f37140955
Hayden McAlister <mcaha814@student.otago.ac.nz>
2023-02-13 16:07:43 +1300

    Update energy functions to remove dep. on NetworkDomain

--- hopfieldnetwork/energyfunction/EnergyFunction.go
+++ hopfieldnetwork/energyfunction/EnergyFunction.go
@@ -1,24 +1,22 @@
 package energyfunction
 
 import (
-	"hmcalister/hopfieldnetwork/hopfieldnetwork/networkdomain"
-
 	"gonum.org/v1/gonum/mat"
 )
 
-// Defines an NetworkEnergyFunction as a function taking a reference to a matrix and a vector
+// Defines an StateEnergyFunction as a function taking a reference to a matrix and a vector
 // that returns a float64 representing the energy.
 //
 // This redefinition is mainly to enforce self documenting code.
-type NetworkEnergyFunction func(*mat.Dense, *mat.VecDense) float64
+type StateEnergyFunction func(*mat.Dense, *mat.VecDense) float64
 
 // Defines UnitEnergyFunction as a function taking a reference to a matrix and a vector
 // as well as an index to note the target unit that
 // returns a float64 representing the energy of the unit
 type UnitEnergyFunction func(*mat.Dense, *mat.VecDense, int) float64
 
-// Defines the UnitEnergy for a binary network
-func UnitBinaryEnergy(matrix *mat.Dense, vector *mat.VecDense, i int) float64 {
+// Defines the energy of a single unit in a vector with respect to a matrix.
+func UnitEnergy(matrix *mat.Dense, vector *mat.VecDense, i int) float64 {
 	dimension, _ := vector.Dims()
 	energy := 0.0
 	for j := 0; j < dimension; j++ {
@@ -27,42 +25,20 @@ func UnitBinaryEnergy(matrix *mat.Dense, vector *mat.VecDense, i int) float64 {
 	return energy
 }
 
-// Defines the overall network energy for a binary network
-func NetworkBinaryEnergy(matrix *mat.Dense, vector *mat.VecDense) float64 {
-	dimension, _ := vector.Dims()
-	energy := 0.0
-	for i := 0; i < dimension; i++ {
-		energy += UnitBinaryEnergy(matrix, vector, i)
+// Defines the overall energy for a state with respect to a matrix.
+func StateEnergy(matrix *mat.Dense, vector *mat.VecDense) float64 {
+	energyVector := AllUnitEnergies(matrix, vector)
+	energy := 0.
+	for i := 0; i < energyVector.Len(); i++ {
+		energy += energyVector.AtVec(i)
 	}
 	return energy
 }
 
-// Defines the UnitEnergy for a binary network
-func UnitBipolarEnergy(matrix *mat.Dense, vector *mat.VecDense, i int) float64 {
-	dimension, _ := vector.Dims()
-	energy := 0.0
-	for j := 0; j < dimension; j++ {
-		energy += -1 * matrix.At(i, j) * vector.AtVec(i) * vector.AtVec(j)
-	}
-	return energy
-}
-
-// Defines the overall network energy for a binary network
-func NetworkBipolarEnergy(matrix *mat.Dense, vector *mat.VecDense) float64 {
-	dimension, _ := vector.Dims()
-	energy := 0.0
-	for i := 0; i < dimension; i++ {
-		energy += UnitBinaryEnergy(matrix, vector, i)
-	}
-	return energy
-}
-
-var DomainToNetworkEnergyFunctionMap = map[networkdomain.NetworkDomain]NetworkEnergyFunction{
-	networkdomain.BinaryDomain:  NetworkBinaryEnergy,
-	networkdomain.BipolarDomain: NetworkBipolarEnergy,
-}
-
-var DomainToUnitEnergyFunctionMap = map[networkdomain.NetworkDomain]UnitEnergyFunction{
-	networkdomain.BinaryDomain:  UnitBinaryEnergy,
-	networkdomain.BipolarDomain: UnitBipolarEnergy,
+func AllUnitEnergies(matrix *mat.Dense, vector *mat.VecDense) *mat.VecDense {
+	energyVector := mat.NewVecDense(vector.Len(), nil)
+	energyVector.MulVec(matrix, vector)
+	energyVector.MulElemVec(energyVector, vector)
+	energyVector.ScaleVec(-1, energyVector)
+	return energyVector
 }
