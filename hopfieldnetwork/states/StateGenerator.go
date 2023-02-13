74e198e7d399673ef74d04cbd868289ad51995af
Hayden McAlister <mcaha814@student.otago.ac.nz>
2023-02-13 16:08:00 +1300

    Update StateGen to improve documentation

--- hopfieldnetwork/states/StateGenerator.go
+++ hopfieldnetwork/states/StateGenerator.go
@@ -22,6 +22,10 @@ type StateGenerator struct {
 // This is implemented for cleaner, more explicit code. Rather than calling NextState once to get memory,
 // this method will create the memory independently. For example, calling AllocStateMemory before a loop and passing
 // the result to NextState within the loop looks a lot nicer than some messy, potentially nil value being passed about.
+//
+// # Returns
+//
+// A new slice of float64 with enough memory to store once VecDense (state).
 func (gen *StateGenerator) AllocStateMemory() []float64 {
 	return make([]float64, gen.dimension)
 }
@@ -29,6 +33,14 @@ func (gen *StateGenerator) AllocStateMemory() []float64 {
 // Create a new random state, using dataArray to store intermediate random values.
 //
 // Note this function require an array to be allocated already. Don't panic! Use AllocDataArray to get this memory!
+//
+// # Arguments
+//
+// * `dataArray`: A slice of float64 with enough memory to store a VecDense. Should be created with AllocStateMemory.
+//
+// # Returns
+//
+// A new state, with backing memory equal to the passed dataArray
 func (gen *StateGenerator) NextState(dataArray []float64) *mat.VecDense {
 	for i := 0; i < gen.dimension; i++ {
 		dataArray[i] = gen.rng.Rand()
@@ -39,9 +51,17 @@ func (gen *StateGenerator) NextState(dataArray []float64) *mat.VecDense {
 	return state
 }
 
-// Creates a set of new states, returning the new states as a pointer to a slice of VecDense
+// Creates a set of new states, returning the new states as a pointer to a slice of VecDense.
 //
 // Note this function does NOT require new memory to be allocated - it is allocated in the method.
+//
+// # Arguments
+//
+// * `numStates` - The number of states to generate.
+//
+// # Returns
+//
+// A slice of new states.
 func (gen *StateGenerator) CreateStateCollection(numStates int) []*mat.VecDense {
 	states := make([]*mat.VecDense, numStates)
 
