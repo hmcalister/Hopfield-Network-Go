56d759885221c4df5c0dc58cf05e05c44dafc1ea
Hayden McAlister <haydenmcalister49@gmail.com>
2023-02-20 23:44:25 +1300

    Change activation function to very explictly compare to (and set values using) floats rather than ints, just in case this was causing an issue

--- hopfieldnetwork/activationfunction/ActivationFunction.go
+++ hopfieldnetwork/activationfunction/ActivationFunction.go
@@ -17,20 +17,20 @@ type ActivationFunction func(*mat.VecDense)
 
 func binaryDomainMappingFunction(vector *mat.VecDense) {
 	for n := 0; n < vector.Len(); n++ {
-		if vector.AtVec(n) <= 0 {
-			vector.SetVec(n, 0)
+		if vector.AtVec(n) <= 0.0 {
+			vector.SetVec(n, 0.0)
 		} else {
-			vector.SetVec(n, 1)
+			vector.SetVec(n, 1.0)
 		}
 	}
 }
 
 func bipolarDomainMappingFunction(vector *mat.VecDense) {
 	for n := 0; n < vector.Len(); n++ {
-		if vector.AtVec(n) <= 0 {
-			vector.SetVec(n, -1)
+		if vector.AtVec(n) <= 0.0 {
+			vector.SetVec(n, -1.0)
 		} else {
-			vector.SetVec(n, 1)
+			vector.SetVec(n, 1.0)
 		}
 	}
 }
