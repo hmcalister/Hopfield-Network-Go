8f3be1e8e503a1239854330c575fbc45cbadc49e
Hayden McAlister <mcaha814@student.otago.ac.nz>
2023-02-24 10:54:28 +1300

    Update analysis script to also look at stability by units updated

--- scripts/main.py
+++ scripts/main.py
@@ -4,10 +4,10 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 FIGSIZE = (10,8)
-trialDataColumns = ["TrialIndex", "UnitsUpdated"]
-trialData = pd.read_parquet("data/trialDataLarge.pq", columns=trialDataColumns)
+trialDataColumns = ["TrialIndex", "UnitsUpdated", "NumberStableStates"]
+trialData = pd.read_parquet("../data/trialData.pq", columns=trialDataColumns)
 stateDataColumns = ['TrialIndex', 'StateIndex', 'Stable', 'NumSteps', "DistancesToLearned"]
-stateData = pd.read_parquet("data/stateDataLarge.pq", columns=stateDataColumns)
+stateData = pd.read_parquet("../data/stateData.pq", columns=stateDataColumns)
 
 joinCol = ["TrialIndex"]
 df = stateData.join(trialData.set_index(joinCol), on=joinCol)
@@ -33,13 +33,34 @@ numStepsDf["Counts"] = numSteps.values
 stableCount = stateData["Stable"].value_counts()
 stableDf = pd.DataFrame()
 stableDf["Stable"] = ["Stable", "Unstable"]
-stableDf["Log Counts"] = np.log10(stableCount.values)
+stableDf["Counts"] = stableCount.values
+
 
 fig = plt.figure(figsize=FIGSIZE)
 ax = fig.add_subplot()
-sns.barplot(stableDf, x="Stable", y="Log Counts", ax=ax)
-plt.title("Log Count of Stable and Unstable States")
+sns.barplot(stableDf, x="Stable", y="Counts", ax=ax)
+plt.title("Count of Stable and Unstable States")
 plt.show()
+
+# -------------------------------------------------------------------------------------------------
+stabilityRatioByUnitsUpdated = trialData.copy()
+stabilityRatioByUnitsUpdated["StabilityRatio"] = stabilityRatioByUnitsUpdated["NumberStableStates"]/1000
+stabilityRatioByUnitsUpdated = stabilityRatioByUnitsUpdated.groupby("UnitsUpdated").aggregate({"StabilityRatio": [np.mean, np.std, "count"]})
+stabilityRatioByUnitsUpdated = stabilityRatioByUnitsUpdated.reset_index()
+stabilityRatioByUnitsUpdated[("StabilityRatio","err")] = \
+    1.96 * stabilityRatioByUnitsUpdated["StabilityRatio"]["std"]/np.sqrt(stabilityRatioByUnitsUpdated["StabilityRatio"]["count"])
+fig = plt.figure(figsize=FIGSIZE)
+plt.errorbar(x=stabilityRatioByUnitsUpdated["UnitsUpdated"], 
+    y=stabilityRatioByUnitsUpdated["StabilityRatio"]["mean"], 
+    yerr=stabilityRatioByUnitsUpdated["StabilityRatio"]["err"], 
+    marker="o",
+    ecolor=(0,0,0,0.3),
+    capsize=3)
+plt.title("Stability Ratio\nby Number of Units Updated")
+plt.xlabel("Number of Units Updated (Per Network Step)")
+plt.ylabel("Stability Ratio")
+plt.show()
+
 
 # -------------------------------------------------------------------------------------------------
 # Minimum Distance to Learned Attractor Distribution (all trials)
@@ -51,6 +72,7 @@ plt.title("Distribution of Minimum Distance to Nearest Learned State\nAll Trials
 plt.xticks(rotation=80)
 plt.tight_layout()
 plt.show()
+
 
 # -------------------------------------------------------------------------------------------------
 # Mean Minimum Distance to Learned Attractor Distribution (grouped by UnitsUpdated)
