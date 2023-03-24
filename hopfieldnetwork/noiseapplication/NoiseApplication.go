30b278dc9e53f6d82001594d52f62e434f8e8929
Hayden McAlister <mcaha814@student.otago.ac.nz>
2023-03-24 15:02:53 +1300

    Add a noise application package to Hopfield Network

---
+++ hopfieldnetwork/noiseapplication/NoiseApplication.go
@@ -0,0 +1,30 @@
+package noiseapplication
+
+import (
+	"hmcalister/hopfield/hopfieldutils"
+	"math"
+
+	"golang.org/x/exp/rand"
+	"gonum.org/v1/gonum/mat"
+)
+
+// Define a function that applies noise to a given state.
+//
+// # Arguments
+//
+// * `randomGenerator`: A random generator used to apply noise to the state
+//
+// * `state`: The state to apply noise to
+//
+// * `noiseScale`: The amount/scale of noise to apply (if applicable).
+type NoiseApplicationMethod func(*rand.Rand, *mat.VecDense, float64)
+
+type NoiseApplicationEnum int
+
+const (
+	None                  NoiseApplicationEnum = iota
+	ExactRatioInversion   NoiseApplicationEnum = iota
+	UniformRatioInversion NoiseApplicationEnum = iota
+	GaussianApplication   NoiseApplicationEnum = iota
+)
+
