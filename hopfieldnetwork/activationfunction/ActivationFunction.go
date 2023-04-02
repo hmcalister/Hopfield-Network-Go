248c655d659b94a7346561d5f8717bc8e5482c6b
Hayden McAlister <mcaha814@student.otago.ac.nz>
2023-04-03 10:35:52 +1200

    Improve activationfunction docstrings

--- hopfieldnetwork/activationfunction/ActivationFunction.go
+++ hopfieldnetwork/activationfunction/ActivationFunction.go
@@ -7,8 +7,17 @@ import (
 	"gonum.org/v1/gonum/mat"
 )
 
+// Defines the activation to be used in the simulation.
+//
+// TODO(hayden): In future, consider a command line flag to set this.
 var ActivationFunction = bipolarDomainMappingFunction
 
+// Apply a bipolar mapping to the given vector. The vector is modified in place.
+// Elements of the vector have their values set to their sign. (0 is mapped to 1.0).
+//
+// # Arguments
+//
+// - vector *mat.VecDense: The vector to apply a bipolar mapping to.
 func bipolarDomainMappingFunction(vector *mat.VecDense) {
 	for n := 0; n < vector.Len(); n++ {
 		if vector.AtVec(n) < 0.0 {
