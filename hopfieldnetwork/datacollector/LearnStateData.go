f8525346bb81aef73fb3fcd48940aaf3ec173843
Hayden McAlister <mcaha814@student.otago.ac.nz>
2023-05-01 11:37:19 +1200

    Add LearnStateData collector

---
+++ hopfieldnetwork/datacollector/LearnStateData.go
@@ -0,0 +1,34 @@
+package datacollector
+
+import (
+	"github.com/xitongsys/parquet-go/writer"
+)
+
+// Representation of the result of a relaxation of a state.
+//
+// State is the state vector that has been relaxed.
+// UnitEnergies is a vector representing the energies of each unit.
+// Stable is a bool representing if the state was stable when relaxation finished.
+// NumSteps is an int representing the number of steps taken when relaxation finished.
+// DistancesToAllLearned is an array of distances to all learned states.
+type LearnStateData struct {
+	Epoch            int       `parquet:"name=Epoch, type=INT32"`
+	TargetStateIndex int       `parquet:"name=TargetStateIndex, type=INT32"`
+	PresentedIndex   int       `parquet:"name=PresentedIndex, type=INT32"`
+	EnergyProfile    []float64 `parquet:"name=EnergyProfile, type=DOUBLE, repetitiontype=REPEATED"`
+}
+
+func NewLearnStateHandler(dataFile string) *dataHandler {
+	fileHandle, dataWriter := newParquetWriter(dataFile, new(LearnStateData))
+	return &dataHandler{
+		eventID:     DataCollectionEvent_LearnState,
+		dataWriter:  dataWriter,
+		fileHandle:  fileHandle,
+		handleEvent: handleLearnStateEvent,
+	}
+}
+
+func handleLearnStateEvent(writer *writer.ParquetWriter, event interface{}) {
+	result := event.(LearnStateData)
+	writer.Write(result)
+}
