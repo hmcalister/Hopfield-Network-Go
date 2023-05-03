9caf6cf532d300a68b017eec75a0956e90e1936a
Hayden McAlister <mcaha814@student.otago.ac.nz>
2023-05-03 15:02:17 +1200

    Upate comments on saving weight matrix

--- main.go
+++ main.go
@@ -171,6 +171,7 @@ func main() {
 		}
 	}
 
+	// Save the weight matrix to the specified path.
 	gonumio.SaveMatrix(network.GetMatrix(), path.Join(*dataDirectory, LEARNED_MATRIX_BINARY_SAVE_FILE))
 
 	// Analyze specifically the learned states and save those results too
