5c6a9d960d00d414c056d9234cfc77836ab4a1f6
Hayden McAlister <mcaha814@student.otago.ac.nz>
2023-05-05 13:12:49 +1200

    Add stable field to data handler struct

--- hopfieldnetwork/datacollector/LearnStateData.go
+++ hopfieldnetwork/datacollector/LearnStateData.go
@@ -15,6 +15,7 @@ type LearnStateData struct {
 	Epoch            int       `parquet:"name=Epoch, type=INT32"`
 	TargetStateIndex int       `parquet:"name=TargetStateIndex, type=INT32"`
 	EnergyProfile    []float64 `parquet:"name=EnergyProfile, type=DOUBLE, repetitiontype=REPEATED"`
+	Stable           bool      `parquet:"name=Stable, type=BOOLEAN"`
 }
 
 func NewLearnStateHandler(dataFile string) *dataHandler {
