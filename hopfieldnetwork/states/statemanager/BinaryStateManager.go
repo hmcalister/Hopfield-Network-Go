1fc0eb636c9dde17b6d9528c85c08989fe6269d4
Hayden McAlister <haydenmcalister49@gmail.com>
2023-06-04 17:23:21 +1200

    Pull reused method of creating constant vector to function

--- hopfieldnetwork/states/statemanager/BinaryStateManager.go
+++ hopfieldnetwork/states/statemanager/BinaryStateManager.go
@@ -5,6 +5,14 @@ import "gonum.org/v1/gonum/mat"
 type BinaryStateManager struct {
 }
 
+func (binaryManager *BinaryStateManager) createCompatibleConstVector(origVector *mat.VecDense, vectorConst float64) *mat.VecDense {
+	constVector := mat.NewVecDense(origVector.Len(), nil)
+	for i := 0; i < constVector.Len(); i++ {
+		constVector.SetVec(i, vectorConst)
+	}
+	return constVector
+}
+
 func (manager *BinaryStateManager) ActivationFunction(vector *mat.VecDense) {
 	for n := 0; n < vector.Len(); n++ {
 		if vector.AtVec(n) <= 0.0 {
@@ -16,10 +24,7 @@ func (manager *BinaryStateManager) ActivationFunction(vector *mat.VecDense) {
 }
 
 func (manager *BinaryStateManager) InvertState(vector *mat.VecDense) {
-	onesVector := mat.NewVecDense(vector.Len(), nil)
-	for i := 0; i < onesVector.Len(); i++ {
-		onesVector.SetVec(i, 1.0)
-	}
+	onesVector := manager.createCompatibleConstVector(vector, 1.0)
 	vector.AddScaledVec(onesVector, -1.0, vector)
 	manager.ActivationFunction(vector)
 }
@@ -30,16 +35,18 @@ func (manager *BinaryStateManager) UnitEnergy(matrix *mat.Dense, vector *mat.Vec
 	dimension, _ := vector.Dims()
 	energy := 0.0
 	for j := 0; j < dimension; j++ {
-		energy += -1 * matrix.At(i, j) * vector.AtVec(i) * vector.AtVec(j)
+		energy += -1 * matrix.At(i, j) * (2*vector.AtVec(i) - 1) * (2*vector.AtVec(j) - 1)
 	}
 	return energy
 }
 
 func (manager *BinaryStateManager) AllUnitEnergies(matrix *mat.Dense, vector *mat.VecDense) []float64 {
+	negativeOnesVector := manager.createCompatibleConstVector(vector, -1.0)
 	energyVector := mat.NewVecDense(vector.Len(), nil)
 	energyVector.MulVec(matrix, vector)
+	energyVector.AddScaledVec(negativeOnesVector, 2.0, energyVector)
 	energyVector.MulElemVec(energyVector, vector)
-	energyVector.ScaleVec(-1, energyVector)
+	energyVector.ScaleVec(-1.0, energyVector)
 	return energyVector.RawVector().Data
 }
 
