2dbaeefb78cbad6ad49e08a2a4bb7734dbdf2ffe
Hayden McAlister <haydenmcalister49@gmail.com>
2023-06-05 15:34:10 +1200

    Add methods to StateManager defining distances between vectors in a domain

--- hopfieldnetwork/states/statemanager/StateManager.go
+++ hopfieldnetwork/states/statemanager/StateManager.go
@@ -12,6 +12,24 @@ type StateManager interface {
 	UnitEnergy(*mat.Dense, *mat.VecDense, int) float64
 	AllUnitEnergies(*mat.Dense, *mat.VecDense) []float64
 	StateEnergy(*mat.Dense, *mat.VecDense) float64
+
+	// Finds the distance between two vectors over a domain
+	MeasureDistance(*mat.VecDense, *mat.VecDense, float64) float64
+
+	// Finds the distances from a given vector to all vectors in a slice
+	//
+	// # Arguments
+	//
+	// vectorCollection: The collection to check over
+	//
+	// vector: The element to check the distance to
+	//
+	// norm: The norm function to use. 1 for Hamming Distance, 2 for Euclidean...
+	//
+	// # Return
+	//
+	// A []float64 representing the distances from the given vector to the slice vectors
+	MeasureDistancesToCollection([]*mat.VecDense, *mat.VecDense, float64) []float64
 }
 
 func GetDomainStateManager(targetDomain domain.DomainEnum) StateManager {
