6e8e2dedf95cab29b935249b06268957434472b9
Hayden McAlister <haydenmcalister49@gmail.com>
2023-06-08 01:24:51 +1200

    Update hopfield network to use distance metric from utils package

--- hopfieldnetwork/HopfieldNetwork.go
+++ hopfieldnetwork/HopfieldNetwork.go
@@ -363,7 +363,7 @@ func (network *HopfieldNetwork) RelaxState(state *mat.VecDense) *RelaxationResul
 		if network.StateIsStable(state) {
 			result := RelaxationResult{
 				Stable:             true,
-				DistancesToTargets: network.domainStateManager.MeasureDistancesToCollection(network.targetStates, state, 1.0),
+				DistancesToTargets: hopfieldutils.MeasureDistancesToCollection(network.targetStates, state, 1.0),
 				StateHistory:       stateHistory[:stepIndex+1],
 				EnergyHistory:      energyHistory[:stepIndex+1],
 			}
@@ -379,7 +379,7 @@ func (network *HopfieldNetwork) RelaxState(state *mat.VecDense) *RelaxationResul
 	energyHistory[len(energyHistory)-1] = network.AllUnitEnergies(state)
 	result := RelaxationResult{
 		Stable:             false,
-		DistancesToTargets: network.domainStateManager.MeasureDistancesToCollection(network.targetStates, state, 1.0),
+		DistancesToTargets: hopfieldutils.MeasureDistancesToCollection(network.targetStates, state, 1.0),
 		StateHistory:       stateHistory,
 		EnergyHistory:      energyHistory,
 	}
@@ -439,7 +439,7 @@ StateRecvLoop:
 			if network.StateIsStable(state) {
 				result := RelaxationResult{
 					Stable:             true,
-					DistancesToTargets: network.domainStateManager.MeasureDistancesToCollection(network.targetStates, state, 1.0),
+					DistancesToTargets: hopfieldutils.MeasureDistancesToCollection(network.targetStates, state, 1.0),
 					StateHistory:       stateHistory[:stepIndex+1],
 					EnergyHistory:      energyHistory[:stepIndex+1],
 				}
@@ -460,7 +460,7 @@ StateRecvLoop:
 		energyHistory[len(energyHistory)-1] = network.AllUnitEnergies(state)
 		result := RelaxationResult{
 			Stable:             false,
-			DistancesToTargets: network.domainStateManager.MeasureDistancesToCollection(network.targetStates, state, 1.0),
+			DistancesToTargets: hopfieldutils.MeasureDistancesToCollection(network.targetStates, state, 1.0),
 			StateHistory:       stateHistory,
 			EnergyHistory:      energyHistory,
 		}
