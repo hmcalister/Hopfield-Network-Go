a066bf68c4c5eda959c8a3faf1a6e673bca386fd
Hayden McAlister <mcaha814@student.otago.ac.nz>
2023-11-23 11:53:12 +1300

    Remove bias term from bipolar state manager

--- hopfieldnetwork/states/statemanager/BipolarStateManager.go
+++ hopfieldnetwork/states/statemanager/BipolarStateManager.go
@@ -22,32 +22,27 @@ func (manager *BipolarStateManager) InvertState(vector *mat.VecDense) {
 	manager.ActivationFunction(vector)
 }
 
-func (manager *BipolarStateManager) UnitEnergy(matrix *mat.Dense, bias *mat.VecDense, vector *mat.VecDense, i int) float64 {
+func (manager *BipolarStateManager) UnitEnergy(matrix *mat.Dense, vector *mat.VecDense, i int) float64 {
 	dimension, _ := vector.Dims()
 	energy := 0.0
 	for j := 0; j < dimension; j++ {
 		energy += -0.5 * matrix.At(i, j) * vector.AtVec(i) * vector.AtVec(j)
 	}
-	energy += -1.0 * vector.AtVec(i) * bias.AtVec(i)
 
 	return energy
 }
 
-func (manager *BipolarStateManager) AllUnitEnergies(matrix *mat.Dense, bias *mat.VecDense, vector *mat.VecDense) []float64 {
+func (manager *BipolarStateManager) AllUnitEnergies(matrix *mat.Dense, vector *mat.VecDense) []float64 {
 	energyVector := mat.NewVecDense(vector.Len(), nil)
 	energyVector.MulVec(matrix, vector)
 	energyVector.MulElemVec(energyVector, vector)
 	energyVector.ScaleVec(-0.5, energyVector)
 
-	biasTerm := mat.NewVecDense(vector.Len(), nil)
-	biasTerm.MulElemVec(bias, vector)
-	energyVector.AddScaledVec(energyVector, -1.0, biasTerm)
-
 	return energyVector.RawVector().Data
 }
 
-func (manager *BipolarStateManager) StateEnergy(matrix *mat.Dense, bias *mat.VecDense, vector *mat.VecDense) float64 {
-	energyVector := manager.AllUnitEnergies(matrix, bias, vector)
+func (manager *BipolarStateManager) StateEnergy(matrix *mat.Dense, vector *mat.VecDense) float64 {
+	energyVector := manager.AllUnitEnergies(matrix, vector)
 	energy := 0.0
 	for _, unitEnergy := range energyVector {
 		energy += unitEnergy
